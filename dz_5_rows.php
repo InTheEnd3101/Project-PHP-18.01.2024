529f47cbf72f2d4eb4d25ba0b7a5c3720a468a78
InTheEnd3101 <wimerv2020@gmail.com>
2024-02-05 21:15:54 +0200

    Added DZ #6

--- dz_5_rows.php
+++ dz_5_rows.php
@@ -9,17 +9,15 @@ if ((mb_strlen($string) <= 20)) {
     echo 'Рядок имеет меньше 20 символов, и проверке не подлежит' . PHP_EOL;
 } else {
     if (substr_count($string, '.') >= 2) {
-            if (($position = mb_strpos($string, '.')) !==false)  {
-                $resultString = trim(mb_substr($string, $position + 1));
-                echo 'Наш результат ►►►:' . PHP_EOL;
-                echo $resultString . PHP_EOL;
-            }
+        if (($position = mb_strpos($string, '.')) !==false)  {
+            $resultString = trim(mb_substr($string, $position + 1));
+            echo 'Наш результат ►►►:' . PHP_EOL;
+            echo $resultString . PHP_EOL;
+        }
     } else {
         echo 'Ваша строка содержит только одно предложение(' . PHP_EOL;
     }
 }
-
-
 
 
 
