ccaeebc8d3e30617415310b532d6eb052a574fae
InTheEnd3101 <wimerv2020@gmail.com>
2024-02-11 17:55:48 +0200

    Added DZ#10

---
+++ dz_10_generator.php
@@ -0,0 +1,28 @@
+<?php
+
+// ДЗ 10. Генератор Фібоначчі
+
+function fibonacciGenerator($maxValue)
+{
+    $current = 0;
+    $next = 1;
+    yield $current;
+
+    while ($current + $next < $maxValue) {
+        $temp = $next;
+        $next = $current + $next;
+        $current = $temp;
+        yield $current;
+    }
+}
+
+function printFibonacciNumbers($maxValue)
+{
+    foreach (fibonacciGenerator($maxValue) as $number) {
+        echo $number . ' ' . PHP_EOL;
+    }
+}
+
+printFibonacciNumbers(100); // Выводим числа Фибоначчи меньше 100
+
+
--- dz_6_function1.php
+++ dz_6_function1.php
@@ -4,32 +4,36 @@
 // 1. Написати програму на PHP, яка містить користувацьку функцію для обчислення площі кола та демонструє використання передачі даних у функцію за допомогою параметрів.
 
 
-$radius = 25;
-function circleArea($radius) {
+/*$radius = 25;
+function circleArea($radius)
+{
     $pi = 3.1415;
     $area = $pi * $radius * $radius;
     return $area;
 }
+
 echo PHP_EOL;
-echo 'Площа круга: ' . circleArea($radius) . PHP_EOL;
+echo 'Площа круга: ' . circleArea($radius) . PHP_EOL;*/
 
 
 // 2. Написати програму на PHP, яка приймає число і підносить його до ступеню.
-/*function exponentiate($base, $exponent) {
+
+function exponentiate($base, $exponent) {
     $result = pow($base, $exponent);
     return $result;
 }
 
 $number = 2;
-$degree = 0;
+$degree = 2;
 
 $result = exponentiate($number, $degree);
 echo PHP_EOL;
-echo 'Ваш результат: ' . $result . PHP_EOL;*/
+echo 'Ваш результат: ' . $result . PHP_EOL;
 
 
 // 3. Використайте функцію в двох варіантах: коли вона повертає нове число і змінює передане.
 // 3.1. Функцію, яка повертає нове число
+
 /*function increaseByOne($number) {
     $newNumber = $number + 1;
     return $newNumber;
@@ -44,7 +48,7 @@ echo $number . PHP_EOL;*/
 // 3.2. Функція, яка змінює передане число
 
 /*function modifyNumber(&$number) {
-=======
+
 function modifyNumber(&$number) {
 
     $number += 1; // Додаємо 1 до значення змінної, переданої за посиланням
